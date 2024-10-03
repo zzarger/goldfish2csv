e05288f6342e5646b7b8178d725b4aa0af7f2e5b
Zain Zarger <zain@pam.hq>
2024-10-02 20:37:31 -0400

    condense massively

--- main.py
+++ main.py
@@ -1,39 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_deck_cards(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    textarea = soup.find('textarea', class_='copy-paste-box')
-    if textarea:
-        return textarea.get_text().split('\n')
-    return []
-
-def clean_deck_cards(cards):
-    # Remove empty items
-    cards = [card for card in cards if card and card.lower() not in ['deck', 'sideboard']]
-    return cards
-
-def expand_deck_cards(cards):
-    expanded_cards = []
-    for card in cards:
-        print(card)
-        parts = card.split(' ', 1)
-        count, card_name = parts
-        expanded_cards.extend([card_name] * int(count))
-    return expanded_cards
-
-
-    
-
 url = 'https://www.mtggoldfish.com/deck/arena_download/6660345'
 
-cards = get_deck_cards(url)
-cards = clean_deck_cards(cards)
-cards = expand_deck_cards(cards)
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
 
-print(cards)
+textarea = soup.find('textarea', class_='copy-paste-box')
+cards = textarea.get_text().split('\n') if textarea else []
+cards = [card for card in cards if card and card.lower() not in ['deck', 'sideboard', 'companion']]
 
+expanded_cards = []
+for card in cards:
+    count, card_name = card.split(' ', 1)
+    expanded_cards.extend([card_name] * int(count))
 
+maindeck = expanded_cards[:-15]
+sideboard = expanded_cards[-15:]
 
+result = {'maindeck': maindeck, 'sideboard': sideboard}
+
+print(result)
